c7ac6600ab1f88cff20f11413824020b9a9b3c08
dell <dell@oraclehost>
2018-07-06 14:44:51 +0100

    hello

--- src/main/java/com/jwt/controller/ClientController.java
+++ src/main/java/com/jwt/controller/ClientController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
